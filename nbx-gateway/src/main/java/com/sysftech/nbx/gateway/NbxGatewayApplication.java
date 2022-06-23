35348e9c42fd5dcb81ae163d073e8e0d543b2c6e
xuyan <xuyan@sysftech.com>
2022-06-23 19:32:25 +0800

    考虑到开发周期比较长，现在以springcloud最新的2022.0.0-M2为基础进行开发。

--- nbx-gateway/src/main/java/com/sysftech/nbx/gateway/NbxGatewayApplication.java
+++ nbx-gateway/src/main/java/com/sysftech/nbx/gateway/NbxGatewayApplication.java
@@ -1,5 +1,12 @@
 package com.sysftech.nbx.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class NbxGatewayApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(NbxGatewayApplication.class, args);
+    }
 }

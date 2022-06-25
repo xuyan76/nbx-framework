8823180ff40051652bcbfe0405fd5e351440f5a7
xuyan <xuyan@sysftech.com>
2022-06-25 18:21:40 +0800

    考虑到开发周期比较长，现在以springcloud最新的2022.0.0-M2为基础进行开发。

---
+++ nbx-business/nbx-order/src/main/java/com/sysftech/nbx/business/order/service/impl/OrderServiceBaseImpl.java
@@ -0,0 +1,10 @@
+package com.sysftech.nbx.business.order.service.impl;
+
+import com.sysftech.nbx.business.order.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class OrderServiceBaseImpl implements OrderService {
+}
